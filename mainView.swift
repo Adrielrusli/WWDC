600d54b0eded67e8f01b7c196c95e2ac4072c3e0
Adrielrusli <Adrielbernard28@yahoo.com>
2023-04-09 11:51:38 +0700

    Initial Commit

---
+++ mainView.swift
@@ -0,0 +1,24 @@
+//
+//  mainView.swift
+//  Terserah
+//
+//  Created by Adriel Bernard Rusli on 06/04/23.
+//
+
+import SwiftUI
+
+struct mainView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+
+struct mainView_Previews: PreviewProvider {
+    static var previews: some View {
+        mainView()
+    }
+}
